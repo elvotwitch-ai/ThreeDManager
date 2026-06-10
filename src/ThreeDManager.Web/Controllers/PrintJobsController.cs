018298ca3e4297874f4f9cb4c6b1915ce4fb67ec
otavio <elvotwitch@gmail.com>
2026-06-10 10:57:57 -0300

    feat: add print job management

--- src/ThreeDManager.Web/Controllers/PrintJobsController.cs
+++ src/ThreeDManager.Web/Controllers/PrintJobsController.cs
@@ -1,6 +1,8 @@
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 using ThreeDManager.Infrastructure.Data;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using ThreeDManager.Domain.Entities;
 
 namespace ThreeDManager.Web.Controllers;
 
@@ -68,5 +70,126 @@ public class PrintJobsController : Controller
             : null;
 
         return View(printJob);
+    }
+    public async Task<IActionResult> Edit(Guid? id)
+    {
+        if (id is null)
+        {
+            return NotFound();
+        }
+
+        var printJob = await _context.PrintJobs.FindAsync(id);
+
+        if (printJob is null)
+        {
+            return NotFound();
+        }
+
+        await PopulateOptionsAsync(printJob);
+
+        return View(printJob);
+    }
+
+    [HttpPost]
+    [ValidateAntiForgeryToken]
+    public async Task<IActionResult> Edit(Guid id, PrintJob printJob)
+    {
+        if (id != printJob.Id)
+        {
+            return BadRequest();
+        }
+
+        if (!ModelState.IsValid)
+        {
+            await PopulateOptionsAsync(printJob);
+            return View(printJob);
+        }
+
+        var existingPrintJob = await _context.PrintJobs.FindAsync(id);
+
+        if (existingPrintJob is null)
+        {
+            return NotFound();
+        }
+
+        existingPrintJob.ProductId = printJob.ProductId;
+        existingPrintJob.PrinterId = printJob.PrinterId;
+        existingPrintJob.MaterialId = printJob.MaterialId;
+        existingPrintJob.FilamentUsedGrams = printJob.FilamentUsedGrams;
+        existingPrintJob.FilamentUsedMeters = printJob.FilamentUsedMeters;
+        existingPrintJob.EstimatedTimeMinutes = printJob.EstimatedTimeMinutes;
+        existingPrintJob.ActualTimeMinutes = printJob.ActualTimeMinutes;
+        existingPrintJob.ReportedCost = printJob.ReportedCost;
+        existingPrintJob.Status = printJob.Status;
+
+        await _context.SaveChangesAsync();
+
+        TempData["SuccessMessage"] = "Produção atualizada com sucesso.";
+
+        return RedirectToAction(nameof(Details), new { id = existingPrintJob.Id });
+    }
+
+    public async Task<IActionResult> Delete(Guid? id)
+    {
+        if (id is null)
+        {
+            return NotFound();
+        }
+
+        var printJob = await _context.PrintJobs
+            .FirstOrDefaultAsync(printJob => printJob.Id == id);
+
+        if (printJob is null)
+        {
+            return NotFound();
+        }
+
+        ViewBag.ProductName = printJob.ProductId.HasValue
+            ? await _context.Products
+                .Where(product => product.Id == printJob.ProductId.Value)
+                .Select(product => product.Name)
+                .FirstOrDefaultAsync()
+            : null;
+
+        return View(printJob);
+    }
+
+    [HttpPost, ActionName("Delete")]
+    [ValidateAntiForgeryToken]
+    public async Task<IActionResult> DeleteConfirmed(Guid id)
+    {
+        var printJob = await _context.PrintJobs.FindAsync(id);
+
+        if (printJob is null)
+        {
+            return NotFound();
+        }
+
+        _context.PrintJobs.Remove(printJob);
+        await _context.SaveChangesAsync();
+
+        TempData["SuccessMessage"] = "Produção removida com sucesso.";
+
+        return RedirectToAction(nameof(Index));
+    }
+
+    private async Task PopulateOptionsAsync(PrintJob printJob)
+    {
+        var products = await _context.Products
+            .Where(product => product.IsActive)
+            .OrderBy(product => product.Name)
+            .ToListAsync();
+
+        var printers = await _context.Printers
+            .OrderBy(printer => printer.Name)
+            .ToListAsync();
+
+        var materials = await _context.Materials
+            .OrderBy(material => material.Name)
+            .ToListAsync();
+
+        ViewBag.ProductOptions = new SelectList(products, "Id", "Name", printJob.ProductId);
+        ViewBag.PrinterOptions = new SelectList(printers, "Id", "Name", printJob.PrinterId);
+        ViewBag.MaterialOptions = new SelectList(materials, "Id", "Name", printJob.MaterialId);
     }
 }
